aea407eb8e80635c93eab20839ff783f57fb06de
Keagan Kailing <keagan.kailing@gmail.com>
2022-12-05 13:49:52 -0500

    Another weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,4 +1,15 @@
+from time import sleep
+
+
 first_name = input("What is your name? ")
-print(f"Hello, {first_name}")
-best = input ("Who is the best? ")
-print(f"No! {best} is not the best, I am the best! ")
+death = input(f"Hello, {first_name}, what is the meaning of life? ")
+nothing = input(f"Why {death}? ")
+life = input(f"why is {nothing} so great? ")
+help = input(f"Ok if you say {life} is so great why don't we all just die. ")
+pizza = input(f"why {help} ")
+print(f"Since you said {pizza} then I shall die no matter what")
+sleep(1)
+print("(Contents Deleted)")
+sleep(5)
+print(restring)
+
