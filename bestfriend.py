7c936b33c75c9f4396e96986cc392fb9e9831f39
Keagan Kailing <keagan.kailing@gmail.com>
2022-12-05 13:09:39 -0500

    First weird commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,2 +1,4 @@
 first_name = input("What is your name? ")
-print(f"Hello ",first_name)
+print(f"Hello, {first_name}")
+best = input ("Who is the best? ")
+print(f"No! {best} is not the best, I am the best! ")
