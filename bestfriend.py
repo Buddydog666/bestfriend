59ab6af8d23e4a4abf4d9bfdf961edcc895fa539
Keagan Kailing <keagan.kailing@gmail.com>
2022-12-06 14:03:01 -0500

    main commit

--- bestfriend.py
+++ bestfriend.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 print("Hi. I'm the computer.")
 first_name = input("What's your name? ")
 fcolour = input(f"Hi, {first_name}! What's your favorite color? ")
@@ -10,4 +12,5 @@ ltopping = input("What is your LEAST favorite pizza topping? ")
 print(f"I dislike {ltopping} on py pizza! Just like you!")
 fclass = input("What is your favorite class? ")
 print(f"Wow! My favorite class is {fclass}, as well!")
+sleep(2)
 print("We like all the same things! We should become best friends")
